0d3ff970abcdf07737e66a40d97f31d62cbed85e
Thomas Klaner <thomas@iwa-tech.com>
2020-03-05 18:55:10 +0100

    add simples usage examples

---
+++ examples/autoabstract.ts
@@ -0,0 +1,14 @@
+import { DefaultApiClient } from '../src';
+
+const client = new DefaultApiClient(process.env.APOLLO_KEY || 'YOUR-API-TOKEN');
+
+client
+  .autoabstract({
+    url: 'https://www.theguardian.com/technology/2017/jan/17/apple-ios-mac-app-store-prices-rise-25-per-cent-following-brexit',
+  })
+  .then(result => {
+    console.group('ApolloAI - autoabstract result');
+    console.dir(result);
+    console.groupEnd();
+  })
+  .catch(console.error);
